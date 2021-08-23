1a288af3f952b40b12468f80dcc1d87327083a0e
Manuel José Martin Mairal <mjmm_manuel@hotmail.com>
2021-08-23 22:46:47 +0200

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-python parlai/scripts/interactive_web.py --task twitter --model-file zoo:blender/blender_90M/model
+python parlai/scripts/interactive_web.py --task twitter --port 8080 --host 0.0.0.0 --model-file zoo:blender/blender_90M/model
